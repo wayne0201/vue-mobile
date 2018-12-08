8aa63d9bd6a8e8df7b29670aab0b7d517eda8db4
wayne0201 <614418910@qq.com>
2018-12-09 00:21:23 +0800

    添加一像素边框与高清图片解决方法

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="border-style">
+      <span>{{text}}</span>
+      <i class="seach-icon"></i>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      text: '1px border'
+    }
   }
 }
 </script>
+
+<style scoped lang="stylus">
+@import "~@/common/stylus/mixin.styl"
+.home
+  display flex
+  flex-direction column
+  justify-content center
+  align-items center
+.border-style
+  display flex
+  align-items center
+  font-size 14px
+  padding px2rem(10px)
+  border-1px(#CCCCCC, 4px)
+  .seach-icon
+    display inline-block
+    margin-left 5px
+    height 16px
+    width 16px
+    background-repeat no-repeat
+    background-position center center
+    background-size 16px 16px
+    bg-image('~@/assets/search')
+</style>
